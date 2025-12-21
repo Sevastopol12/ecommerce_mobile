c702988f9712c099e0bb7a04f1e25bb47358ae18
Sevastopol12 <chkalov253@gmail.com>
2025-12-21 13:17:49 +0700

    feat(homepage): add database connection

--- app/src/main/java/lab/week/buchs/ui/BookListActivity.java
+++ app/src/main/java/lab/week/buchs/ui/BookListActivity.java
@@ -29,6 +29,7 @@ public class BookListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
 
